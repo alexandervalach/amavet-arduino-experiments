d856dd108670d543bac2b31836b5b53e098f1c53
Alexander Valach <alexander.valach@gmail.com>
2021-04-05 20:19:53 +0200

    Fundamental code for accelerometer

--- 03 - Detekcia pádu/03 - Detekcia pádu.ino
+++ 03 - Detekcia pádu/03 - Detekcia pádu.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MPU6050_tockn.h>
 
+#define BUZZER_PIN 4
+#define BUTTON_PIN 3
+#define DEVICE_DEBUG 1
+#define LED_PIN 13
+
+MPU6050 mpu6050(Wire);
+volatile byte state = LOW;
+
+void setup() {
+  #if DEVICE_DEBUG
+    // UART init
+    Serial.begin(9600);
+  #endif
+
+  // I2C init
+  Wire.begin();
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_state, CHANGE);
+  
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, state);
+  mpu6050.update();  
+}
 
+void button_state () {
+  state = !state;
 }
